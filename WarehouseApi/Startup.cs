be3b92c12502d3b882304a41345b5abcb0d4de1a
Roxana Lazar <roxana.lazar@evozon.com>
2021-05-07 14:22:45 +0300

    Create and migrate database at Startup

--- WarehouseApi/Startup.cs
+++ WarehouseApi/Startup.cs
@@ -47,8 +47,10 @@ namespace WarehouseApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WarehouseContext context)
         {
+            context.Database.EnsureCreated();
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
